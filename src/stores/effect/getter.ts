af1bdb68fab3e8ce566e57df85668742e0e264ee
Sylvain Garnot <sylvain.garnot@gmail.com>
2024-09-21 10:46:28 +0200

    store remove effect

--- src/stores/effect/getter.ts
+++
@@ -1,6 +0,0 @@
-import { computed } from 'vue';
-import { effect } from './state';
-
-export const effectSrc = computed(() => {
-  return effect.value.src as string;
-})
--- src/stores/effect/state.ts
+++
@@ -1,5 +0,0 @@
-import { ref } from 'vue';
-
-export const effect = ref(new Audio as HTMLAudioElement);
-export const effectVolume = ref(0.5 as number);
-export const isEffectPlaying = ref(false as boolean);
