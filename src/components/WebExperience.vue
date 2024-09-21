597c0d679c6a9dea54796f3a46eb1357c6bfd25c
Sylvain Garnot <sylvain.garnot@gmail.com>
2024-09-21 13:29:02 +0200

    init webexp

--- src/components/WebExperience.vue
+++ src/components/WebExperience.vue
@@ -1,52 +1,51 @@
 <template>
-  <div :class="route.name === 'home' ? 'container' : ''">
-    <div id="webxp" class="ep-webxp" data-webxp-author="webxp" data-webxp-id="j8vvd53"
-      :class="route.name === 'home' ? 'active' : ''"></div>
+  <div class="container">
+    <div id="webxp" class="ep-webxp" data-webxp-author="webxp" data-webxp-id="j8vvd53"></div>
+    test gfsd gfdsg
+    <!-- <WebExperienceCharacterCall v-for="bookmarkToast in bookmarkToasts" :height="bookmarkToast.height"
+      :width="bookmarkToast.width" :top="bookmarkToast.top" :left="bookmarkToast.left" /> -->
 
-    <VskBookmarkToast v-for="bookmarkToast in bookmarkToasts" :height="bookmarkToast.height"
-      :width="bookmarkToast.width" :top="bookmarkToast.top" :left="bookmarkToast.left" />
-
-    <VskDialog />
+    <!-- <WebExperienceCharacterDialog /> -->
 
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 import { onMounted, onUpdated } from "vue";
 import { useRoute } from 'vue-router';
 import { storeToRefs } from 'pinia'
 
-import VskBookmarkToast from "@/layouts/VskBookmarkToast.vue";
-import VskDialog from "@/layouts/VskDialog.vue";
+// import WebExperienceCharacterCall from "@/components/WebExperienceCharacterCall.vue";
+// import WebExperienceCharacterDialog from "@/components/WebExperienceCharacterDialog.vue";
 
 import { useApi } from "./WebExperienceApi.js";
 
-import { useToastStore } from "@/stores/toast/index.ts";
-import { useBookmarkToastStore } from "@/stores/character/index.js";
-import { useLocationStore } from "@/stores/location";
-import type { locationsGlossaryInterface } from '@/stores/location/interface';
+// import { useToastStore } from "@/stores/toast/index.ts";
+// import { useBookmarkToastStore } from "@/stores/character/index.js";
+// import { useLocationStore } from "@/stores/location";
+// import type { locationsGlossaryInterface } from '@/stores/location/interface';
 
 
 const route = useRoute();
 
 const { EndlessPaper } = useApi();
 
-const toastStore = useToastStore();
-const { addToast } = toastStore;
+// const toastStore = useToastStore();
+// const { addToast } = toastStore;
 
-const bookmarkToastStore = useBookmarkToastStore();
-const { bookmarkToasts } = storeToRefs(bookmarkToastStore);
+// const bookmarkToastStore = useBookmarkToastStore();
+// const { bookmarkToasts } = storeToRefs(bookmarkToastStore);
 
-const locationStore = useLocationStore();
-const { locationsGlossary } = locationStore;
-const { updateNearbyLocations } = locationStore;
+// const locationStore = useLocationStore();
+// const { locationsGlossary } = locationStore;
+// const { updateNearbyLocations } = locationStore;
 
 
 
 // UPDATED
 onUpdated(() => {
   if (route?.query?.location) {
-    teleportTo(route?.query?.location);
+    // teleportTo(route?.query?.location);
   }
 });
 
@@ -57,15 +56,15 @@ onMounted(() => {
 
     EndlessPaper.showNavBar(false);
     EndlessPaper.showTravelButtons(false);
-    EndlessPaper.onBookmarkNearby(updateNearbyLocations);
+    EndlessPaper.onBookmarkNearby(test);
 
     setTimeout(() => {
-      const locationGlossary = locationsGlossary[0] as locationsGlossaryInterface;
-      const tostMessage = locationGlossary.placeToText.en ? locationGlossary.placeToText.en : locationGlossary.name as string;
-      addToast(tostMessage, "success", 2000);
+      // const locationGlossary = locationsGlossary[0] as locationsGlossaryInterface;
+      // const tostMessage = locationGlossary.placeToText.en ? locationGlossary.placeToText.en : locationGlossary.name as string;
+      // addToast(tostMessage, "success", 2000);
 
       if (route?.query?.location) {
-        teleportTo(route?.query?.location);
+        //   teleportTo(route?.query?.location);
       }
     }, 900);
   });
@@ -73,29 +72,30 @@ onMounted(() => {
 
 
 // METHODS
-function teleportTo(location: string) {
-  if (locationsGlossary.find(l => l.name === location) as locationsGlossaryInterface) {
-    EndlessPaper.visitBookmark(location as string);
-  } else {
-    addToast('Localisation introuvable', "error", 2000);
-  }
-
+function teleportTo(location) {
+  //   if (locationsGlossary.find(l => l.name === location) as locationsGlossaryInterface) {
+  //     EndlessPaper.visitBookmark(location as string);
+  //   } else {
+  //     // addToast('Localisation introuvable', "error", 2000);
+  //   }
 }
+function test() {
+  console.log('TEST HEY');
+}
+
 </script>
 
 <style lang="scss" scoped>
-.ep-webxp {
-  // border: solid yellow 2px;
+.container {
+  background-color: #666666;
+  position: relative;
   width: 100%;
-  overflow: hidden;
+  height: 100vh;
 
-  height: 0;
-  opacity: 0;
-  transition: opacity 2s ease-in;
-
-  &.active {
+  .ep-webxp {
+    width: 100%;
     height: 100%;
-    opacity: 1;
+    overflow: hidden;
   }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,15 @@
 <template>
   <div>
-    <div id="body-container">
-      <WebExperience />
-      <TheMenu />
-      <VskToast />
-    </div>
+    <WebExperience />
+    <!-- <TheMenu /> -->
+    <!-- <VskToast /> -->
   </div>
 </template>
 
 <script setup>
 import WebExperience from "@/components/WebExperience.vue";
-import TheMenu from "@/layouts/TheMenu.vue";
-import VskToast from "@/layouts/VskToast.vue";
+// import TheMenu from "@/layouts/TheMenu.vue";
+// import VskToast from "@/layouts/VskToast.vue";
 </script>
 
-<style scoped lang="scss">
-#body-container {
-  /* border: solid 1px red; */
-  overflow-y: scroll;
-  background-color: #666666;
-}
-</style>
+<style scoped lang="scss"></style>
