8dc30c2fb107467ec7f861f7a45535d79e4075c0
Sylvain Garnot <sylvain.garnot@gmail.com>
2024-09-22 23:11:20 +0200

    intro toast message

--- src/components/WebExperience.vue
+++ src/components/WebExperience.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <div id="webxp" class="ep-webxp" data-webxp-author="optitest" data-webxp-id="j8vvd53"></div>
+
     <!-- <WebExperienceCharacterCall v-for="bookmarkToast in bookmarkToasts" :height="bookmarkToast.height"
       :width="bookmarkToast.width" :top="bookmarkToast.top" :left="bookmarkToast.left" /> -->
 
@@ -15,35 +16,30 @@ import { onMounted, onUpdated } from "vue";
 import { useRoute } from 'vue-router';
 import { storeToRefs } from 'pinia';
 
-
 // import WebExperienceCharacterCall from "@/components/WebExperienceCharacterCall.vue";
 import WebExperienceCharacterDialog from "@/components/WebExperienceCharacterDialog.vue";
 
 import { useApi } from "./WebExperienceApi.js";
-
 import { useBookmarkStore } from "@/stores/bookmark/index.ts";
 import { useLocationStore } from "@/stores/location/index.ts";
-import { useCharacterStore } from "@/stores/character/index.ts";
-
 
 import { useDialogStore } from "@/stores/dialog";
-const dialogStore = useDialogStore();
-const { dialog, isDialogActive } = storeToRefs(dialogStore);
-
+import { useToastStore } from "@/stores/toast";
 
 const route = useRoute();
-
 const { EndlessPaper } = useApi();
-
-// const toastStore = useToastStore();
-// const { addToast } = toastStore;
 
 const bookmarkStore = useBookmarkStore();
 const { updateNearbyBookmarks } = bookmarkStore;
-// const { bookmarks } = storeToRefs(bookmarkStore);
 
 const locationStore = useLocationStore();
-const characterStore = useCharacterStore();
+const { locations } = storeToRefs(locationStore);;
+
+const dialogStore = useDialogStore();
+const { dialog, isDialogActive } = storeToRefs(dialogStore);
+
+const toastStore = useToastStore();
+const { addToast } = toastStore;
 
 // UPDATED
 onUpdated(() => {
@@ -62,12 +58,10 @@ onMounted(() => {
     EndlessPaper.onBookmarkNearby(updateNearbyBookmarks);
 
     setTimeout(() => {
-      // const locationGlossary = locationsGlossary[0] as locationsGlossaryInterface;
-      // const tostMessage = locationGlossary.placeToText.en ? locationGlossary.placeToText.en : locationGlossary.name as string;
-      // addToast(tostMessage, "success", 2000);
+      addToast(locations.value[0].message, "info");
 
       if (route?.query?.location) {
-        //   teleportTo(route?.query?.location);
+        // teleportTo(route?.query?.location);
       }
     }, 900);
   });
