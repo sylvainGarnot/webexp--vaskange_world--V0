2eab0900ef48bcef3c2033f8a140b7760c0e3edb
Sylvain Garnot <sylvain.garnot@gmail.com>
2024-10-08 21:49:14 +0200

    Inventory with thumbnail card

--- src/components/Inventory.vue
+++ src/components/Inventory.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <VskBtn image="joystick" @click="isActive = !isActive" />
+    <VskBtn image="joystick" :disable="items_acquired!.length === 0"
+      @click="items_acquired!.length > 0 ? isActive = !isActive : ''" />
 
     <v-dialog class="vsk-map-v-dialog" v-model="isActive">
       <template v-slot:default>
@@ -10,7 +11,7 @@
             <!-- TITRE -->
             <v-row no-gutters class="vsk-map-title mt-12">
               <v-col cols="12">
-                <h3>Lieux découverts</h3>
+                <h3>Objets Trouvés</h3>
               </v-col>
             </v-row>
 
@@ -21,13 +22,12 @@
             <!-- CONTENT -->
             <v-row no-gutters class="vsk-map-locations px-3 pb-3">
               <TransitionGroup name="list-animation" class="transition-group-element" tag="div">
-                <VskThumbnail v-for="locationFound in locations_foundSorted" :key="locationFound.id"
-                  class="transition-group-element" :title="locationFound.name" :link="locationFound.name"
-                  :subtitle="locationFound?.nbrItemsToAcquired > 0 ? `secrets trouvé ${locationFound?.nbrItemsAcquired} / ${locationFound?.nbrItemsToAcquired}` : ''"
-                  :imageUrl="`${locationFound.image_url}`" @router-push="isActive = false" />
-                <VskThumbnail v-if="locationsFoundProgression.length < locations.length" title="À découvrir..."
+                <VskThumbnailCard v-for="itemAcquired in items_acquiredSorted" :key="itemAcquired.id"
+                  class="transition-group-element" :title="itemAcquired.name" :description="itemAcquired.description"
+                  :imageUrl="`${itemAcquired.image_url}`" @router-push="isActive = false" />
+                <VskThumbnail v-if="items_acquired.length < items.length" title="À découvrir..."
                   class="transition-group-element" :imageUrl="`/images/location/secret_place.PNG`"
-                  :key="locationsFoundProgression.length" />
+                  :key="items_acquired.length" />
               </TransitionGroup>
             </v-row>
           </div>
@@ -44,23 +44,17 @@
 import { ref, computed } from 'vue';
 import { storeToRefs } from 'pinia'
 import VskThumbnail from '@/layouts/VskThumbnail.vue'
+import VskThumbnailCard from '@/layouts/VskThumbnailCard.vue'
 import VskBtn from '@/layouts/VskBtn.vue'
 import VskSwitchGroup from '@/layouts/VskSwitchGroup.vue'
 
-import { useLocationStore } from "@/stores/location"
-import type { locationFoundProgressionInterface } from '@/stores/location/interface';
+import { useItemStore } from "@/stores/item"
+import type { itemAcquiredInterface } from '@/stores/item/interface';
 
-import { useCharacterStore } from "@/stores/character"
-import type { characterFoundInterface } from '@/stores/character/interface';
-
-const locationStore = useLocationStore()
-const { locationsFoundProgression, locations } = storeToRefs(locationStore)
-
-const characterStore = useCharacterStore()
-const { characters_found, characters } = storeToRefs(characterStore)
+const itemStore = useItemStore()
+const { items_acquired, items } = storeToRefs(itemStore)
 
 const isActive = ref(false);
-const tab = ref('tabs-option-1');
 
 const sortTypes = ref([
   {
@@ -96,8 +90,8 @@ const sortType = computed(() => {
   }
 })
 
-const locations_foundSorted = computed(() => {
-  const result = [...locationsFoundProgression.value] as locationFoundProgressionInterface[];
+const items_acquiredSorted = computed(() => {
+  const result = [...items_acquired.value] as itemAcquiredInterface[];
   if (sortType.value === 'défaut') {
     return result.sort((a, b) => (parseInt(a.id) - parseInt(b.id)));
   }
@@ -105,7 +99,7 @@ const locations_foundSorted = computed(() => {
     return result.sort((a, b) => (a.name < b.name ? -1 : 1));
   }
   else if (sortType.value === 'date') {
-    return result.sort((a, b) => (a.found_date < b.found_date ? -1 : 1));
+    return result.sort((a, b) => (a.acquired_date < b.acquired_date ? -1 : 1));
   }
 })
 
@@ -114,7 +108,7 @@ const locations_foundSorted = computed(() => {
 <style lang="scss" scoped>
 .vsk-map-close {
   position: absolute;
-  right: 1.7vh;
+  right: 1.4vh;
   top: -50vh + 15vh + 1.3vh;
   cursor: pointer;
 
---
+++ src/layouts/VskThumbnailCard.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="vsk-thumbnail-card mt-3">
+    <v-row no-gutters class="mt-2">
+      <v-col cols="4">
+        <v-img class="vsk-thumbnail-card-image" :src="imageUrl">
+        </v-img>
+      </v-col>
+      <v-col cols="8">
+        <v-card-title class="vsk-thumbnail-card-title">
+          {{ title }}
+          <v-divider :thickness="1" class="border-opacity-50 mt-3" color="grey"></v-divider>
+        </v-card-title>
+        <v-card-text class="vsk-thumbnail-card-description mt-1">
+          {{ description }}
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  title: String,
+  imageUrl: String,
+  description: String,
+})
+</script>
+
+<style lang="scss" scoped>
+.vsk-thumbnail-card {
+  width: 100%;
+  height: 12vh;
+  border-radius: 0.8vh;
+  position: relative;
+  border: solid 1px grey;
+
+  .vsk-thumbnail-card-image {
+    width: 75%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+
+  .vsk-thumbnail-card-title {
+    color: white;
+    font-size: 2.3vh;
+    font-weight: 700;
+    line-height: 1;
+  }
+
+  .vsk-thumbnail-card-description {
+    color: white;
+    font-size: 1.4vh;
+    font-weight: 300;
+    line-height: 1.2;
+  }
+
+}
+</style>
