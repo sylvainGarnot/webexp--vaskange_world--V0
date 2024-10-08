7759310a2f4f57270c7264047908ac4fdcd006f3
Sylvain Garnot <sylvain.garnot@gmail.com>
2024-10-08 20:41:05 +0200

    Vsk Btn with disable option

--- src/layouts/VskBtn.vue
+++ src/layouts/VskBtn.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <button class="btn-menu">
+    <button class="vsk-btn" :class="disable ? 'disable' : 'active'">
+      <div v-if="disable" class="vsk-btn-disable-background"></div>
       <img class="icon" :src="`/icones/${image}.png`" />
     </button>
   </div>
@@ -9,20 +10,40 @@
 <script setup>
 defineProps({
   image: String,
+  disable: Boolean,
 })
 </script>
 
 <style lang="scss" scoped>
-.btn-menu {
+.vsk-btn {
   width: 5.8vh;
   height: 5.8vh;
   max-width: 65px;
   max-height: 65px;
 
-  background-color: rgba(29, 27, 25, 0.8);
   border-radius: 0.8vh;
   padding: 0.4vh;
-  cursor: pointer;
+  background-color: rgba(29, 27, 25, 0.8);
+
+  &.active {
+    cursor: pointer;
+  }
+
+  position: relative;
+
+  &.disable {
+    cursor: not-allowed;
+
+    .vsk-btn-disable-background {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      border-radius: 0.8vh;
+      background-color: rgba(255, 255, 255, 0.454);
+    }
+  }
 
   img {
     width: 100%;
