c1338c913e87d67495a8d8e198dbb2ec0688c7fd
Sylvain Garnot <sylvain.garnot@gmail.com>
2024-10-13 14:30:41 +0200

    test music for iOS

--- src/stores/music/state.ts
+++ src/stores/music/state.ts
@@ -10,37 +10,37 @@ export const isMusicPlaying = ref(false as boolean);
 export const musics = ref([
   {
     id: '0',
-    file: '/music/free/coccibar 🐞.aac',
+    file: 'https://prismatic-concha-27bd5b.netlify.app/music/free/coccibar 🐞.aac',
     audio: new Audio,
   },
   {
     id: '1',
-    file: '/music/free/fin quest 1 🔥.aac',
+    file: 'https://prismatic-concha-27bd5b.netlify.app/music/free/fin quest 1 🔥.aac',
     audio: new Audio,
   },
   {
     id: '2',
-    file: '/music/free/matrix 📺.aac',
+    file: 'https://prismatic-concha-27bd5b.netlify.app/music/free/matrix 📺.aac',
     audio: new Audio,
   },
   {
     id: '3',
-    file: '/music/free/star night club ⭐️.aac',
+    file: 'https://prismatic-concha-27bd5b.netlify.app/music/free/star night club ⭐️.aac',
     audio: new Audio,
   },
   {
     id: '4',
-    file: '/music/free/support city 🛖.aac',
+    file: 'https://prismatic-concha-27bd5b.netlify.app/music/free/support city 🛖.aac',
     audio: new Audio,
   },
   {
     id: '5',
-    file: '/music/free/temple 🏯.aac',
+    file: 'https://prismatic-concha-27bd5b.netlify.app/music/free/temple 🏯.aac',
     audio: new Audio,
   },
   {
     id: '6',
-    file: '/music/free/trésor 💰.aac',
+    file: 'https://prismatic-concha-27bd5b.netlify.app/music/free/trésor 💰.aac',
     audio: new Audio,
   },
 ] as musicInterface[]);
