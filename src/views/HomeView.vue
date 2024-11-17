6d6c4f446cd6134c1f8a4fe7eb4e92ae89365b2b
Sylvain Garnot <sylvain.garnot@gmail.com>
2024-11-17 17:30:19 +0100

    retirer nom des lieu en toast

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,14 @@
   <div>
     <WebExperience />
     <TheMenu />
-    <VskToast />
+    <!-- <VskToast /> -->
   </div>
 </template>
 
 <script setup>
 import WebExperience from "@/components/WebExperience.vue";
 import TheMenu from "@/layouts/TheMenu.vue";
-import VskToast from "@/layouts/VskToast.vue";
+// import VskToast from "@/layouts/VskToast.vue";
 </script>
 
 <style scoped lang="scss"></style>
