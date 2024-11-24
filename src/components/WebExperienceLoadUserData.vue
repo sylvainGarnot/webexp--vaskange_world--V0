570a708702848cb67f41dd807fbe93ecdafac004
Sylvain Garnot <sylvain.garnot@gmail.com>
2024-11-24 19:39:14 +0100

    loadData Progress loader

--- src/components/WebExperienceLoadUserData.vue
+++ src/components/WebExperienceLoadUserData.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
+
+    <div v-if="loading" class="vsk-load-user-data--progression-overlay">
+      <div class="vsk-load-user-data--progression-container">
+        <v-row no-gutters class="vsk-load-user-data--progression">
+          <v-col cols="12" align="center">
+            <p>Récupération de vos données</p>
+          </v-col>
+          <v-col cols="12" class="mt-3">
+            <v-progress-linear height="16" indeterminate rounded></v-progress-linear>
+          </v-col>
+        </v-row>
+      </div>
+    </div>
+
+
     <VskCard :isActive="isActive" @update:is-active="$event => $emit('update:isActive', $event)"
       @close="handleReprendre()">
       <template v-slot:content>
 
-        <v-row no-gutters class="message mt-4">
+        <v-row no-gutters class="vsk-load-user-data--message mt-4">
           <v-col cols="12" align="center">
             <h3>Bon retour !</h3>
           </v-col>
@@ -25,6 +40,7 @@
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
 import VskCard from '@/layouts/VskCard.vue'
 import { setLocationFoundFromCookies, setDefaultLocationFound } from '@/stores/location/action';
 import { setCharacterFoundFromCookies } from '@/stores/character/action';
@@ -38,7 +54,12 @@ const props = defineProps({
   isActive: Boolean,
 })
 
+const loading = ref(false);
+
+
 function handleReprendre() {
+  loading.value = true
+
   setLocationFoundFromCookies()
 
   setTimeout(() => {
@@ -46,9 +67,10 @@ function handleReprendre() {
 
     setTimeout(() => {
       setItemAcquiredFromCookies()
-    }, 2000);
+      loading.value = false
+    }, 2250);
 
-  }, 2000);
+  }, 2250);
   playMusic()
   emit('update:isActive', false)
 }
@@ -64,7 +86,7 @@ function handleRecommencer() {
 <style lang="scss" scoped>
 @import '@/assets/styles/_global_variable.scss';
 
-.message {
+.vsk-load-user-data--message {
   color: $colorWhite;
 
   h3 {
@@ -74,5 +96,41 @@ function handleRecommencer() {
   p {
     font-size: 2.1vh;
   }
+}
+
+.vsk-load-user-data--progression-overlay {
+  position: fixed;
+  overflow-y: scroll;
+  inset: 0;
+  background-color: $colorBlackLightUltra;
+
+  .vsk-load-user-data--progression-container {
+    width: 500px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    .vsk-load-user-data--progression {
+      color: $colorWhite;
+      font-size: 2.2vh;
+      font-weight: 700;
+      text-shadow: $shadow;
+    }
+  }
+
+}
+</style>
+
+<style lang="scss">
+@import '@/assets/styles/_global_variable.scss';
+
+.v-progress-linear__background {
+  opacity: 0.35;
+  background-color: $colorBlack;
+}
+
+.v-progress-linear__indeterminate {
+  background-color: $colorGold;
 }
 </style>
