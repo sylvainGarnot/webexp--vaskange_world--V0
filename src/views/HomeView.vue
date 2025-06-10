f0afbf4c133a5a374c325b943b892c1ae34a7fc1
Sylvain Garnot <sylvain.garnot@gmail.com>
2025-06-10 15:33:12 +0200

    remove guide

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,12 @@
   <div>
     <WebExperience />
     <TheMenu />
-    <Guide />
   </div>
 </template>
 
 <script setup>
 import WebExperience from "@/components/WebExperience.vue";
 import TheMenu from "@/layouts/TheMenu.vue";
-import Guide from "@/components/Guide.vue";
 </script>
 
 <style scoped lang="scss"></style>
